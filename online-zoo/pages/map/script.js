a452110fdbda8ea74d7ff8a6bda6b26bd44bed56
frostwOw13 <frostwow13@yandex.ru>
2021-04-03 22:29:16 +0300

    fix: fix nav in map js page

--- online-zoo/pages/map/script.js
+++ online-zoo/pages/map/script.js
@@ -1,5 +1,5 @@
 // Navigation bar first element is active or not
-const navElem = document.getElementById("linkHeader2");
+const navElem = document.getElementById("linkHeader3");
 navElem.addEventListener("mouseover", e => {
     navElem.classList.toggle("active");
 });
@@ -17,30 +17,3 @@ switcher.addEventListener("click", e => {
         darkTheme = false;
     }
 })
-
-// Aside script
-const activePet = document.querySelectorAll(".pets-slider__image");
-const arrowUp = document.querySelector(".arrow-up");
-const arrowDown = document.querySelector(".arrow-down");
-
-activePet.forEach((pet, id) => {
-    if (pet.classList.contains("active")) {   // Check which pet have active class
-        if (id === 0) {
-            arrowUp.style.marginBottom = "0";    // Set margin to 0, for good block display
-        } else if (id === 3) {
-            arrowDown.style.marginTop = "0";  // Set margin to 0, for good block display
-        }
-    }
-})
-
-// Pets player script
-const switchButton = document.querySelectorAll(".pets-player__watch-switch__item");
-
-switchButton.forEach(btn => {
-    btn.addEventListener("click", e => {
-        switchButton.forEach(btn1 => {
-            btn1.classList.remove("active"); // Set all btn to non-active
-        });
-        btn.classList.toggle("active"); // Active clicked btn
-    });
-});
