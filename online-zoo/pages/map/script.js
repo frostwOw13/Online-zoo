72bc491598e8a7b301fa65f36de3279bb9caa83a
frostwOw13 <frostwow13@yandex.ru>
2021-04-08 20:43:38 +0300

    fix: fix path to icon switcher

--- online-zoo/pages/map/script.js
+++ online-zoo/pages/map/script.js
@@ -4,10 +4,10 @@ let darkTheme = false; // boolean var to track dark theme;
 
 switcher.addEventListener("click", e => {
     if (darkTheme === false) {
-        switcher.src = "../../../assets/images/switcher__theme_dark.png";
+        switcher.src = "../../assets/images/switcher__theme_dark.png";
         darkTheme = true;
     } else {
-        switcher.src = "../../../assets/images/switcher__theme.png";
+        switcher.src = "../../assets/images/switcher__theme.png";
         darkTheme = false;
     }
 })
